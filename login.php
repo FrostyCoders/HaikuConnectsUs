5a5c8d081341af6dfcb1b5739190aa3654b8c0c6
Git-Gud-Dude <pl.krzysiek.pl.pl@gmail.com>
2020-04-11 13:47:57 +0200

    Created login site

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+    session_start();
+    if(isset($_SESSION['haiku']) && $_SESSION['haiku'] == true)
+    {
+        header("Location: main_page.php");
+        exit();
+    }
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Haiku ConnectUs - Login</title>
+    <meta name="author" content="Frosty Coders">
+    <link rel="shortcut icon" href="img/icon.png">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <div class="window">
+        <div class="frame">
+            <div class="logo"><img src="img/logo.png" alt="Haiku Logo"></div>
+            <form action="login.php" method="POST">
+                <p class="login_label">E-mail</p>
+                <input type="text" name="fp-login" required>
+                <p class="password_label">Password</p>
+                <input type="password" name="fp-password" required><br>
+                <p class="pass_forgot"><a href="forgotpasswd.php">Forgot Password</a></p>
+                 <br><br>
+                <input type="submit" value="Login">
+                <p class="login_error">
+                    <?php
+                        if(isset($_SESSION['login_error']))
+                        {
+                            echo $_SESSION['login_error'];
+                            unset($_SESSION['login_error']);
+                        }
+                    ?>
+                </p>
+            </form>
+        </div>
+    </div>
+    <div class="copy">Copyright &copy - Frosty Coders 2020</div>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
