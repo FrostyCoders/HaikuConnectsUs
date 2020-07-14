b224e4bc3678b386594eb6e4ca559ced76ab6128
farriven <legend4rny@gmail.com>
2020-07-14 13:42:18 +0200

    Add live image preview

--- public/js/add_haiku_live.js
+++ public/js/add_haiku_live.js
@@ -4,12 +4,11 @@ function liveAddHaiku()
     var inEnglish = document.getElementById("in-english").value;
     var inNative = document.getElementById("in-native").value;
     var isChecked = document.getElementById("ischecked").checked;
-    var background;
 
     var postAuthor = document.getElementById("post-author");
     postAuthor.textContent = author;
-    var postHaiku = document.getElementById("post-haiku");
     
+    var postHaiku = document.getElementById("post-haiku");
     if(isChecked == false)
         {
             postHaiku.textContent = inEnglish;
@@ -19,12 +18,39 @@ function liveAddHaiku()
             postHaiku.textContent = inNative;
         }
     
-    setTimeout(liveLoop, 1000);
-}
 
-function liveLoop()
-{
+    
+    liveCheckEnter();
     setTimeout(liveAddHaiku, 1000);
 }
 
-liveLoop();
+function liveCheckEnter()
+{
+    var inEnglish = document.getElementById("in-english").value;
+    var inEnglishs = document.getElementById("in-english");
+    var inNative = document.getElementById("in-native").value;
+    var postHaiku = document.getElementById("post-haiku");
+    if (inEnglish.keyCode === 13)
+        {
+            inEnglish = inEnglish + "/n";
+            postHaiku.textContent = inEnglish;
+        }
+} 
+
+var pullfiles = function()
+{ 
+    var fileInput = document.querySelector("#background-haiku");
+    
+    var reader = new FileReader();
+    reader.onload = function()
+    {
+        var postHeader = document.getElementById("post-header"); 
+        postHeader.style.backgroundImage = 'url('+reader.result+')';
+    }
+    
+    reader.readAsDataURL(fileInput.files[0]) 
+}
+
+liveAddHaiku();
+
+document.querySelector("#background-haiku").onchange=pullfiles;
