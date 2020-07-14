18ee866b001a49f26200866d381d3349326697cc
farriven <legend4rny@gmail.com>
2020-07-14 20:40:23 +0200

    Finish prototype of live view

--- public/js/add_haiku_live.js
+++ public/js/add_haiku_live.js
@@ -1,42 +1,57 @@
-function liveAddHaiku()
+// CHANGE AUTHOR ON LIVE
+function liveAuthorHaiku()
 {    
     var author = document.getElementById("author").value;
-    var inEnglish = document.getElementById("in-english").value;
-    var inNative = document.getElementById("in-native").value;
-    var isChecked = document.getElementById("ischecked").checked;
-
     var postAuthor = document.getElementById("post-author");
     postAuthor.textContent = author;
     
-    var postHaiku = document.getElementById("post-haiku");
-    if(isChecked == false)
-        {
-            postHaiku.textContent = inEnglish;
-        }
-    else if(isChecked == true)
-        {
-            postHaiku.textContent = inNative;
-        }
-    
-
-    
     liveCheckEnter();
-    setTimeout(liveAddHaiku, 1000);
 }
 
+// CHANGE HAIKU POST ON LIVE
 function liveCheckEnter()
 {
     var inEnglish = document.getElementById("in-english").value;
-    var inEnglishs = document.getElementById("in-english");
     var inNative = document.getElementById("in-native").value;
     var postHaiku = document.getElementById("post-haiku");
-    if (inEnglish.keyCode === 13)
+    var isChecked = document.getElementById("ischecked").checked;
+   
+    var inEnglishArray = Array.from(inEnglish);
+    var inNativeArray = Array.from(inNative);
+    
+    postHaiku.textContent = "";
+    
+    if(isChecked == false)
         {
-            inEnglish = inEnglish + "/n";
-            postHaiku.textContent = inEnglish;
+            inEnglishArray.forEach(element => {
+            if(element.charCodeAt(0) == 10)
+                {
+                    postHaiku.innerHTML += "<br />";
+                }
+            else
+                {
+                    postHaiku.innerHTML += element;
+                }
+            });
         }
+    else if(isChecked == true)
+        {
+            inNativeArray.forEach(element => {
+            if(element.charCodeAt(0) == 10)
+                {
+                    postHaiku.innerHTML += "<br />";
+                }
+            else
+                {
+                    postHaiku.innerHTML += element;
+                }
+            });
+        }
+
+    setTimeout(liveAuthorHaiku, 1000);
 } 
 
+// CHANGE BACKGROUND ON LIVE
 var pullfiles = function()
 { 
     var fileInput = document.querySelector("#background-haiku");
@@ -51,6 +66,6 @@ var pullfiles = function()
     reader.readAsDataURL(fileInput.files[0]) 
 }
 
-liveAddHaiku();
+liveAuthorHaiku();
 
 document.querySelector("#background-haiku").onchange=pullfiles;
