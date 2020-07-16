acb6128f92da9eb60e18659a121ad56776c66f63
farriven <legend4rny@gmail.com>
2020-07-16 10:06:22 +0200

    Add tabs: About The Project and Contact

--- public/js/main.js
+++ public/js/main.js
@@ -3,16 +3,18 @@
 function showHandwriting()
 {
     var handContainer = document.getElementById("post-nav-handwriting");
-    if(handContainer.style.display==="block")
-        {
-            handContainer.style.display = "none";
-        }
-    else
-        {
-            handContainer.style.display = "block";
-            handContainer.style.animation = "show-element 1s 1";
-        }
+    handContainer.style.display = "block";
+    handContainer.style.animation = "show-element 1s 1";
 }
 
 var handOption = document.getElementById("post-nav-sub-option-handwriting");
 handOption.addEventListener('click', showHandwriting, false);
+
+function hideHandwriting()
+{
+    var handContainer = document.getElementById("post-nav-handwriting");
+    handContainer.style.display = "none";
+}
+
+var handOptionClose = document.getElementById("post-nav-handwriting-close");
+handOptionClose.addEventListener('click', hideHandwriting, false);
